6dbde520ea7f51032a239b7c21c29d435dc2d397
youliso <youliso@hotmail.com>
2023-10-12 10:13:45 +0000

    update

--- web/views/pages/home/index.vue
+++ web/views/pages/home/index.vue
@@ -2,6 +2,12 @@
   <div class='info'>
     <h1>hello</h1>
     <button class="btn">Button</button>
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-ghost">Ghost</button>
+    <button class="btn btn-link">Link</button>
   </div>
 </template>
 <script lang='ts'>
@@ -10,10 +16,14 @@ import { windowShow } from '@youliso/electronic/ipc';
 
 export default defineComponent({
   setup() {
+
+
     onMounted(() => {
       // @ts-ignore
       window.ipc && windowShow();
     });
+
+
     return {};
   }
 });
