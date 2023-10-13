e9a185474ecd9f362325fa3792450240e2a9dfe2
youliso <youliso@hotmail.com>
2023-10-13 01:22:07 +0000

    update

--- web/views/pages/home/index.vue
+++ web/views/pages/home/index.vue
@@ -1,13 +1,6 @@
 <template>
   <div class='info'>
-    <h1>hello</h1>
-    <button class="btn">Button</button>
-    <button class="btn btn-neutral">Neutral</button>
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-accent">Accent</button>
-    <button class="btn btn-ghost">Ghost</button>
-    <button class="btn btn-link">Link</button>
+    <h1>capacitance</h1>
   </div>
 </template>
 <script lang='ts'>
