4326ca2441d5e20a631c5da0435c125358a79b77
youliso <youliso@hotmail.com>
2023-10-20 10:12:28 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./web/index.html",
-    "./web/**/*.{vue,js,ts}"
-  ],
-  plugins: [require("daisyui")],
-}
+  content: ['./web/index.html', './web/**/*.{vue,js,ts}'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark', 'cupcake']
+  }
+};
